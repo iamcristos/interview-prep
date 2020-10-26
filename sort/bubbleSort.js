8c1d7155926b82474b9359bfc8b845f438f16e4c
iamcristos <nmereginivincent@gmail.com>
2020-10-26 20:59:03 +0100

    completed buble sort

---
+++ sort/bubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(data) {
+    let swap;
+    let length = data.length - 1
+    do {
+        swap = false;
+        for (let i = 0; i < length; i++) {
+           if(data[i] < data[i+1]) {
+               console.log(i)
+            const temp = data[i]
+            data[i] = data[i+1]
+            data[i+1] = temp
+            swap = true
+           }
+        }
+    } while (swap)
+    return data;
+}
+
+console.log(bubbleSort([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]));
